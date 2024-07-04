c3c1d5722281de32e80a77cc0cb40d2201453da6
dkrasnov <Denis.Krasnov@artezio.com>
2024-07-04 15:27:34 +0300

    2: 7.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import NavMenu from './components/NavMenu/NavMenu';
 import MainPage from "./components/MainPage/MainPage";
 
 function App() {
+  const [title] = useState("SLEEPCOIN");
   return (
     <div className="App">
-      <NavMenu />
+      <NavMenu title={title}/>
       <MainPage />
     </div>
   );
--- src/components/NavMenu/NavMenu.tsx
+++ src/components/NavMenu/NavMenu.tsx
@@ -1,34 +1,15 @@
 import React from 'react'
-import Logo from '../../images/icons8-sos-or-emergency-phone-no-layout-with-hand-receiver-96.png'
 
-const NavMenu = () => {
+const NavMenu = (props: any) => {
 
   return (
-    <div className="flex flex-row border border-b-blue-500 justify-between">
-      <div className="h-full">
-        <button className="bg-sky-500 rounded w-full my-1">
-          First left button
-        </button>
-        <button className="bg-sky-500 rounded w-full my-1">
-          Second left button
-        </button>
-        <button className="bg-sky-500 rounded w-full my-1">
-          Third left button
-        </button>
-      </div>
-      <img src={Logo}  alt={'Logo'}/>
-      <div className="h-full">
-        <button className="bg-sky-500 rounded w-full my-1">
-          First right button
-        </button>
-        <button className="bg-sky-500 rounded w-full my-1">
-          Second right button
-        </button>
-        <button className="bg-sky-500 rounded w-full my-1">
-          Third right button
-        </button>
-      </div>
-    </div>
+      <nav className="navbar navbar-dark bg-dark">
+        <div className="container">
+          <div className="row m-auto">
+            <div className="text-light h2">{props.title}</div>
+          </div>
+        </div>
+      </nav>
   )
 }
 
